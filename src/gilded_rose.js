22fb91202e8414eeb7d483c5b1c0d042d52b5bef
Neil Campbell <neildcampbell@hotmail.com>
2019-10-26 06:35:17 +0100

    refactoring into different functions

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -12,46 +12,54 @@ class Shop {
   }
   updateQuality() {
     this.items.forEach(function(item){
+      function reduceQualityBy1(){item.quality--;}
+      function increaseQualityBy1(){item.quality++}
+      function reduceSellInBy1 (){item.sellIn--}
+      function notLegendary(){if (item.name != 'Sulfuras, Hand of Ragnaros') {
+        reduceQualityBy1()
+      }}
+      function backStagePasses(){if (item.sellIn < 11) {
+        if (item.quality < 50) {
+          increaseQualityBy1()
+        }
+      }}
+
+      function reduceQualityToZero(){item.quality = 0;}
+      
       if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (item.quality > 0) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            item.quality = item.quality - 1;
-          }
+          notLegendary()
         }
       } else {
         if (item.quality < 50) {
-          item.quality = item.quality + 1;
+          increaseQualityBy1()
           if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
+            backStagePasses()
             if (item.sellIn < 6) {
               if (item.quality < 50) {
-                item.quality = item.quality + 1;
+                increaseQualityBy1()
               }
             }
           }
         }
       }
       if (item.name != 'Sulfuras, Hand of Ragnaros') {
-        item.sellIn = item.sellIn - 1;
+        reduceSellInBy1()
       }
       if (item.sellIn < 0) {
         if (item.name != 'Aged Brie') {
           if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (item.quality > 0) {
               if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                item.quality = item.quality - 1;
+                reduceQualityBy1();
               }
             }
           } else {
-            item.quality = item.quality - item.quality;
+            reduceQualityToZero()
           }
         } else {
           if (item.quality < 50) {
-            item.quality = item.quality + 1;
+            increaseQualityBy1();
           }
         }
       }
