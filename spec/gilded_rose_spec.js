47853e9d5a3e169f0c108caaa2c6a1f008de54c0
Neil Campbell <neildcampbell@hotmail.com>
2019-10-27 06:21:08 +0000

    replaced numbers with const statements

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,4 +1,8 @@
 describe("Gilded Rose", function() {
+  const MAX_QUALITY = 50;
+  const MIN_QUALITY = 0
+  const MIN_SELLIN = 0
+  const QUALITY_DECREASE_STANDARD_VALUE = 1
   it("should foo", function() {
     const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
     const items = gildedRose.updateQuality();
@@ -7,7 +11,7 @@ describe("Gilded Rose", function() {
   it("should reduce in quality after each day by 1 ", function(){
     const gildedRose = new Shop([new Item("foo", 5, 10)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].quality).toEqual(9)
+    expect(items[0].quality).toEqual(10 - QUALITY_DECREASE_STANDARD_VALUE)
   })
   it('should reduce in quantity by 1 when update quality', function(){
     const gildedRose = new Shop([new Item("foo", 5, 10)]);
@@ -17,7 +21,7 @@ describe("Gilded Rose", function() {
   it('should ensure the quality is never a negative number', function(){
     const gildedRose = new Shop([new Item("foo", 50, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].quality).toEqual(0)
+    expect(items[0].quality).toEqual(MIN_QUALITY)
   })
   it('should ensure the quality of Aged Brie increases every day', function(){
     const gildedRose = new Shop([new Item("Aged Brie", 24, 1)]);
@@ -27,17 +31,17 @@ describe("Gilded Rose", function() {
   it('should ensure the quality of an item is not more than 50', function(){
     const gildedRose = new Shop([new Item("Aged Brie", 10, 50)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].quality).toEqual(50)
+    expect(items[0].quality).toEqual(MAX_QUALITY)
   })
   it('Once the sell by date has passed, Quality degrades twice as fast', function(){
     const gildedRose = new Shop([new Item("Shoes", 0, 50)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].quality).toEqual(48)
+    expect(items[0].quality).toEqual(MAX_QUALITY - 2)
   })
   it('Sulfuras”, being a legendary item, never has to be sold or decreases in Quality', function(){
     const gildedRose = new Shop([new Item('Sulfuras, Hand of Ragnaros', 10, 50)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].quality).toEqual(50)
+    expect(items[0].quality).toEqual(MAX_QUALITY)
     expect(items[0].sellIn).toEqual(10)
   })
   it('Backstage passes increase in quality by 2 if there are 10 days or less', function(){
@@ -53,6 +57,7 @@ describe("Gilded Rose", function() {
   it('Backstage passes drop to 0 in quality after concert', function(){
     const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 0, 40)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].quality).toEqual(0)
+    expect(items[0].quality).toEqual(MIN_QUALITY)
   })
+  
 });
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -10,31 +10,36 @@ class Shop {
   constructor(items=[]){
     this.items = items;
   }
+
+  
   updateQuality() {
+    const MAX_QUALITY = 50;
+    const MIN_QUALITY = 0
+    const MIN_SELLIN = 0
     this.items.forEach(function(item){     
-      function reduceQualityBy1(){item.quality--;}
-      function increaseQualityBy1(){item.quality++}
-      function reduceSellInBy1 (){item.sellIn--}
+      let reduceQualityBy1 = () => item.quality --
+      let increaseQualityBy1 = () => item.quality ++
+      let reduceSellInBy1 = () => item.sellIn --
 
       if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert' && item.name != 'Sulfuras, Hand of Ragnaros' ) {
-       if(item.quality > 0 && item.quality < 50 && item.sellIn > 0 ){
+       if(item.quality > MIN_QUALITY && item.quality < MAX_QUALITY && item.sellIn > MIN_SELLIN ){
         reduceQualityBy1()
         reduceSellInBy1()
-      } else if (item.sellIn <=0){
-        item.quality-=2
+      } else if (item.sellIn <= MIN_SELLIN){
+        item.quality -= 2
       }
     }
     if (item.name == "Aged Brie"){
-      if(item.quality < 50){
+      if(item.quality < MAX_QUALITY){
         increaseQualityBy1()
       }
     }
 
     if(item.name === 'Backstage passes to a TAFKAL80ETC concert'){
-      if(item.sellIn > 0 && item.sellIn < 6){
-        item.quality+=3
+      if(item.sellIn > MIN_SELLIN && item.sellIn < 6){
+        item.quality += 3
       } else if(item.sellIn >=6 && item.sellIn <= 10){
-        item.quality +=2
+        item.quality += 2
       } else if(item.sellIn <= 0){
         item.quality = 0;
       } else if (item.sellIn > 10){
